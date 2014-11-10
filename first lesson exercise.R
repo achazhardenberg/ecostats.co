2d1fd76f4fde0839cc199d03b367b5c2af88cba5
Achaz von Hardenberg <achaz.hardenberg@gmail.com>
2014-11-10 09:21:09 +0100

    added video to lesson 4

---
+++ first lesson exercise.R
@@ -0,0 +1,34 @@
+# This is the exercise script of the second lesson
+# author: Achaz von Hardenberg
+# date: 27-10-2014 
+# v1.0
+
+setwd("~/Documents/ecostats_practice")
+
+a <- 1+2 # this is how you assign a value
+b = 2+4
+a+b #I can just sum up assigned values
+a
+c <- "Hello World!"
+print(c)
+c
+b <- 89
+
+# How to find help?
+
+help(mean)
+?mean
+x <- c(a,b)
+x
+mean(x)
+
+help.search("regression")
+
+#to know where my working directory is
+getwd()
+
+
+
+
+
+
